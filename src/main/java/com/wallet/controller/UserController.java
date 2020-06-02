410ddcdf13c2482406d2d82792f9192b6523e75b
LarissaZanata <zanatalarissamorais@gmail.com>
2020-06-01 21:47:42 -0300

    Criando UserController e seus testes

--- src/main/java/com/wallet/controller/UserController.java
+++ src/main/java/com/wallet/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.wallet.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wallet.dto.UserDTO;
 import com.wallet.entity.User;
+import com.wallet.response.Response;
 import com.wallet.service.UserService;
 
 @RestController
@@ -18,10 +24,12 @@ public class UserController {
 	private UserService service;
 	
 	@PostMapping
-	public ResponseEntity<Response<UserDTO>> created(@Valid @Request UserDTO dto, BrindingResult result){
+	public ResponseEntity<Response<UserDTO>> created(@Valid @RequestBody UserDTO dto, BindingResult result){
 		
 		Response <UserDTO> response = new Response<UserDTO>();
+		
 		User user = service.save(this.convertDtoToEntity(dto));
+		
 		response.setData(this.convertEntityToDto(user));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
